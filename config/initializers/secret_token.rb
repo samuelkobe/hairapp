29b6282af71e95eae564a3926370d1a26497d6a3
Samuel Kobe <samuelkobe@gmail.com>
2012-09-14 15:53:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hairapp::Application.config.secret_token = '42aa129c75478b0a7aefd72044e451022188eeb0f5c454a2db89af6ad2d69edebe8840c013b7867c52057d1a4212da1b708860c93c97c263fa540690baaa3c1b'
